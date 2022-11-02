9ca92592798b1068bf83cb8f7d85ffd9c15ae525
Braulin Pires <100023268+Braulin96@users.noreply.github.com>
2022-11-02 15:10:38 +0000

    add the ModulewihtOptions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Module from "./components/Module";
+import ModuleWithOptions from "./components/ModuleWithOptions";
+      <ModuleWithOptions />
 function App() {
   return (
+  
     <div className="App">
       <Module />
+     <ModuleWithOptions />
     </div>
   );
 }
