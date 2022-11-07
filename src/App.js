f003776567055a28517848c385ac8cd100e80e42
Braulin Pires <100023268+Braulin96@users.noreply.github.com>
2022-11-07 20:51:55 +0000

    ModuleWithOptios done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import Module from "./components/Module";
 import ModuleWithOptions from "./components/ModuleWithOptions";
+import Example from "./components/Example"
 
-const style={
-  h3: {
-    color:"black",
-    margin:"20px 0px 0px 0px", 
-  },
-  h4: {
-    color:"black",
-    margin:"5px 0px 0px 0px", 
-  }
-}
 
 function App() {
   return (
   <div >
-    <Module></Module>
-    <ModuleWithOptions/>
+    <ModuleWithOptions>
+      <h3>98%</h3>
+      <p>Health Score</p>
+    </ModuleWithOptions>
+
   </div> 
   );
 }
--- src/components/ModuleWithOptions.jsx
+++ src/components/ModuleWithOptions.jsx
@@ -5,14 +5,20 @@ import '@szhsin/react-menu/dist/index.css';
 import '@szhsin/react-menu/dist/transitions/slide.css';
 import optionIcon from '../images/optionIcon.svg'
 import secondIcon from '../images/secondIcon.svg'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Example from './Example';
+import { Alert } from 'bootstrap';
 
 const stylesIcon={
   moduleIcon: {
     backgroundColor:"white",
     border:"none",
-    margin:" 10px 0px 10px 50px ", 
+    padding:" 15px 0px 10px 65px ", 
     width: '5px',
     height:'5px',
+  
   },
   moduleDotIcon: {
     margin:"5px 0px 0px 0px",
@@ -22,28 +28,52 @@ const stylesIcon={
     margin:"0px 0px 0px 15px",
     lineHeight:"3px",
     fontSize:"15px",
-  }
+  },
 }
 
+
 const ModuleWithOptions =(props) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  console.log(props)
+
   return ( 
+    <>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Woohoo, you're reading this text in a modal!
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal> 
+
     <Module>
       <div style={stylesIcon.moduleDotIcon}>
         <img src={secondIcon} alt="image" />
       </div>
       <div>
         <Menu menuButton ={<MenuButton style={stylesIcon.moduleIcon}><img src={optionIcon} alt="image"/></MenuButton>} transition>
-          <MenuItem>New File</MenuItem>
-          <MenuItem>Save</MenuItem>
-          <MenuItem>Close Window</MenuItem>
+          <MenuItem><a onClick={handleShow}><li >Learn more</li></a></MenuItem>
+          <MenuItem ><li>Action</li></MenuItem>
         </Menu>
-        {props.children}
+        
       </div>
       <div style={stylesIcon.containerText}>
-        <h3>98%</h3>
-        <h4 >Health Score</h4>
+        {props.children}
       </div>
     </Module>
+    </>
   )
 }
 export default ModuleWithOptions
