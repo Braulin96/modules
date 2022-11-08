59247915ba2240b52d5e9205288175b3797a874d
Braulin Pires <100023268+Braulin96@users.noreply.github.com>
2022-11-08 17:29:19 +0000

    Carousel updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import ModuleWithOptions from "./components/ModuleWithOptions";
-import AppExample from "./components/AppExample";
+import Carousel from 'react-elastic-carousel';
 
 function App() {
+  const breakPoints=[
+    { width: 1, itemsToShow: 1},
+    { width: 100, itemsToShow: 2},
+    { width: 500, itemsToShow: 3},
+    { width: 700, itemsToShow: 4},
+]
   return (
   <div >
-    {/*
-    <ModuleWithOptions>
-      <h3>98%</h3>
-      <p>Health Score</p>
-    </ModuleWithOptions>
-    */ }
-    <AppExample/>
-    
+    <Carousel breakPoints={breakPoints}>
+      <ModuleWithOptions>
+        <h3>2/7</h3>
+        <p>Checks</p>
+      </ModuleWithOptions>
+      <ModuleWithOptions>
+        <h3>98%</h3>
+        <p>Health Score</p>
+      </ModuleWithOptions>
+      <ModuleWithOptions>
+        <h3>Dr Lyunch</h3>
+        <p>My specialist</p>
+      </ModuleWithOptions>
+      <ModuleWithOptions>
+        <h3>Fitness</h3>
+        <p>My hours</p>
+      </ModuleWithOptions>
+      <ModuleWithOptions>
+        <h3>Medical History</h3>
+      </ModuleWithOptions>
+    </Carousel>
   </div> 
   );
 }
