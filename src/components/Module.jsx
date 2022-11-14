579fc8eeed411be335ca76715f8de65c08d5afbf
Braulin Pires <braulinpires@hotmail.com>
2022-11-14 15:55:36 +0000

    calendar Week concluded

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const stylesOptions={
   module: {
-   // display:"flex",
-  //  flexWrap:"wrap",
+    // display:"flex",
+    //  flexWrap:"wrap",
     borderRadius:'15px',
     background: 'none',
     boxShadow: 'rgba(249, 157, 165, 0.2) 0px 8px 24px',
     width: 'fit-content',
     height: 'fit-content',
-}
+  }
 }
 
 const Module =(props) =>{
