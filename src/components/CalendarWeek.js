8fd87a22825abfef19f442acf3d397ba67b055d7
Braulin Pires <braulinpires@hotmail.com>
2022-11-18 00:14:42 +0000

    calendar week

--- src/components/CalendarWeek.js
+++ src/components/CalendarWeek.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ModuleCalendar from './ModuleCalendar'
+import { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import weekIcon from '../images/weekIcon.svg'
+import monthIcon from '../images/monthIcon.svg'
 
 function CalendarWeek() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const style={
         dayOfWeek:{
             fontSize: "0.75rem"
@@ -15,51 +23,75 @@ function CalendarWeek() {
         }
     } 
   return (
-    <div style={{position: "relative"}} className='my-3 d-flex flex-wrap w-100 justify-content-between '>
-        <ModuleCalendar >
-            <a onClick={() => console.log("hello")} style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Mon</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p>  
-            </a>
-        </ModuleCalendar>
-        <ModuleCalendar>
-            <div style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Tue</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p> 
-            </div>
-        </ModuleCalendar>
-        <ModuleCalendar>
-            <div style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Wed</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p>   
-            </div>
-        </ModuleCalendar>
-        <ModuleCalendar>
-            <div style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Thu</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p>  
-            </div> 
-        </ModuleCalendar>
-        <ModuleCalendar>
-            <div style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Fri</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p>
-            </div>   
-        </ModuleCalendar>
-        <ModuleCalendar>
-            <div style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Sat</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p>   
-            </div>
-        </ModuleCalendar>
-        <ModuleCalendar>
-            <div style={style.containerSize}>
-                <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Sun</p>
-                <p className='text-center mb-0 p-0 pb-0'>7</p>  
-            </div>
-        </ModuleCalendar>
-   
-    </div> 
+    <>
+        <Modal size='sm' show={show} onHide={handleClose} centered>
+            <Modal.Body>
+                <div style={{padding:"20px 20px"}} >
+                    <div className=' d-flex justify-content-between flex-wrap'> 
+                        <div>
+                            <img width={40} style={{marginLeft:"5px"}} src={weekIcon} alt="image" />
+                        </div>
+                        <div>
+                            <h3 style={{paddingTop:"5px"}}> <b> Week view </b></h3>
+                        </div>
+                    </div>
+                    <div className=' d-flex justify-content-between flex-wrap'>
+                        <div>
+                            <img width={47} src={monthIcon} alt="image" />
+                        </div>
+                        <div>
+                            <h3 style={{paddingTop:"5px"}}>Month view</h3>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal> 
+        <div style={{position: "relative"}} className='my-3 d-flex flex-wrap w-100 justify-content-between '>
+            <ModuleCalendar >
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Mon</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p>  
+                </div>
+            </ModuleCalendar>
+            <ModuleCalendar>
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Tue</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p> 
+                </div>
+            </ModuleCalendar>
+            <ModuleCalendar>
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Wed</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p>   
+                </div>
+            </ModuleCalendar>
+            <ModuleCalendar>
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Thu</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p>  
+                </div> 
+            </ModuleCalendar>
+            <ModuleCalendar>
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Fri</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p>
+                </div>   
+            </ModuleCalendar>
+            <ModuleCalendar>
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Sat</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p>   
+                </div>
+            </ModuleCalendar>
+            <ModuleCalendar>
+                <div a onClick={handleShow} style={style.containerSize}>
+                    <p className='p-0 mb-0 pb-1 text-center' style={style.dayOfWeek}>Sun</p>
+                    <p className='text-center mb-0 p-0 pb-0'>7</p>  
+                </div>
+            </ModuleCalendar>
+    
+        </div> 
+    </>
   )
 }
 export default CalendarWeek
